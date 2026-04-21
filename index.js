dee9cb4ebc15f74268de13d4f9d97c003a22b3f5
Yash <yashsharda606@gmail.com>
2026-04-21 14:29:45 +0530

    all

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
-})
+});
 
 app.get("/about", (req, res) => {
   res.render("about.ejs");
